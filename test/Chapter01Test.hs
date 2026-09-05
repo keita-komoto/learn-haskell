8bf9fde8517c2584a0315e317255a809df31126b
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-05 18:10:54 +0900

    tests: add PBT with Hedgehog

--- test/Chapter01Test.hs
+++ test/Chapter01Test.hs
@@ -1,7 +1,15 @@
 module Chapter01Test where
 
+import Chapter01
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.HUnit
-import Chapter01
 
 unit_doubleMeTest = doubleMe 2 @?= 4
+
+hprop_alwaysDoubled :: Property
+hprop_alwaysDoubled = property $ do
+  x <- forAll $ Gen.int (Range.linearFrom 0 (-100) 100)
+  doubleMe x === 2 * x
