c8348816ad624ab6e6d85ffccc8e14195458c6af
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-08 16:35:33 +0900

    chapter05: quicksort

---
+++ app/chapter05.hs
@@ -0,0 +1,11 @@
+module Chapter05 where
+
+randomIntItems = [10, 2, 3, 5, 1, 4, 6, 9, 8, 7]
+stringItems = ["the quick brown fox jumps over the lazy dog"]
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+   in smallerSorted ++ [x] ++ biggerSorted
