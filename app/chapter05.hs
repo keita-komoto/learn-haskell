a2f17765f71ab26efebbcc311cc2cf39b22c2457
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-08 17:58:50 +0900

    lint: apply format

--- app/chapter05.hs
+++ app/chapter05.hs
@@ -1,6 +1,7 @@
 module Chapter05 where
 
 randomIntItems = [10, 2, 3, 5, 1, 4, 6, 9, 8, 7]
+
 stringItems = ["the quick brown fox jumps over the lazy dog"]
 
 quicksort :: Ord a => [a] -> [a]
@@ -8,7 +9,7 @@ quicksort [] = []
 quicksort (x : xs) =
   let smallerSorted = quicksort [a | a <- xs, a <= x]
       biggerSorted = quicksort [a | a <- xs, a > x]
-   in smallerSorted ++ [x] ++ biggerSorted
+  in smallerSorted ++ [x] ++ biggerSorted
 
 compareWithHundred :: Int -> Ordering
 compareWithHundred = compare 100
--- test/Chapter05Test.hs
+++ test/Chapter05Test.hs
@@ -8,11 +8,14 @@ import Test.Tasty
 import Test.Tasty.HUnit hiding (assert)
 
 unit_quicksortIsMinOrderInt = quicksort randomIntItems @?= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 unit_quicksortIsMinOrderString = quicksort stringItems @?= ["the quick brown fox jumps over the lazy dog"]
+
 hprop_quicksortIsMinOrderInt = property $ do
   xs <- forAll $ Gen.list (Range.linear 0 100) (Gen.int (Range.linearFrom 0 (-100) 100))
   let result = quicksort xs
   assert (and (zipWith (<=) result (drop 1 result)))
+
 hprop_quicksortIsMinOrderString = property $ do
   xs <- forAll $ Gen.list (Range.linear 0 100) (Gen.list (Range.linear 0 100) Gen.alpha)
   let result = quicksort xs
