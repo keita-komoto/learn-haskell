cda781c8362c2329c45c7fe6253531a7667037c2
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-16 17:13:30 +0900

    chapter06: find needle in haystack

--- app/Chapter06.hs
+++ app/Chapter06.hs
@@ -15,3 +15,11 @@ wawee = "wa wa wee wa wee"
 -- [("wa",3),("wee",2)]
 wordNum :: String -> [(String, Int)]
 wordNum = map (\ws -> (head ws, length ws)) . L.group . L.sort . L.words
+
+-- | Find something in array
+-- >>> "art" `isIn` "party"
+-- True
+-- >>> [1,2] `isIn` [1,3,5]
+-- False
+isIn :: Eq a => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `L.isPrefixOf`) (L.tails haystack)
