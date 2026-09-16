01dd6297c430e5fe669c1c9ae2b675e114b5bffa
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-16 12:12:27 +0900

    chapter05: remove lambda from sum function

--- app/Chapter05.hs
+++ app/Chapter05.hs
@@ -75,4 +75,5 @@ numLongChains :: Int
 numLongChains = length (filter (\xs -> length xs > 15) (map chain [1 .. 100]))
 
 sum' :: Num a => [a] -> a
-sum' xs = foldl (\acc x -> acc + x) 0 xs
+sum' = foldl (+) 0
+
