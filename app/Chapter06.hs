9b16328389bfc68d681ca376b2e0e81baabb23a7
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-16 19:47:12 +0900

    chapter06: refactor use folding

--- app/Chapter06.hs
+++ app/Chapter06.hs
@@ -65,6 +65,4 @@ phoneBook =
 -- Nothing
 findKey :: Eq k => k -> [(k, v)] -> Maybe v
 findKey key [] = Nothing
-findKey key ((k, v) : xs)
-  | key == k = Just v
-  | otherwise = findKey key xs
+findKey key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
