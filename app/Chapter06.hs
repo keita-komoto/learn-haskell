af956ed9ba320a617de530bd5815dfbb8dbeb294
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-19 18:19:26 +0900

    refactor: to be oneline

--- app/Chapter06.hs
+++ app/Chapter06.hs
@@ -103,6 +103,4 @@ phoneBook'' =
 -- >>> M.lookup "patsy" $ phoneBookToMap phoneBook''
 -- Just "555-3333, 222-5555, 333-4444"
 phoneBookToMap :: Ord k => [(k, String)] -> M.Map k String
-phoneBookToMap = M.fromListWith add
-  where
-    add number1 number2 = number1 ++ ", " ++ number2
+phoneBookToMap = M.fromListWith add where add number1 number2 = number1 ++ ", " ++ number2
