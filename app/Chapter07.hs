89396ed47c86f97ed0e5c3a5de9783a0e71d3c73
keita-komoto <125565087+keita-komoto@users.noreply.github.com>
2026-09-19 19:35:39 +0900

    chapter07: calculate the area of a rectangle using data types and value constructors

--- app/Chapter07.hs
+++ app/Chapter07.hs
@@ -1,1 +1,14 @@
 module Chapter07 where
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+
+-- | Calculate the area of a circle
+-- >>> area $ Circle 10 20 10
+-- 314.15927
+
+-- | Calculate the area of a rectangle
+-- >>> area $ Rectangle 0 0 100 100
+-- 10000.0
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r ^ 2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
